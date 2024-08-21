9e54a68cf7bbd8b8af75b762db74fef82b979209
Felix Fontein <felix@fontein.de>
2024-08-21 15:32:37 +0200

    Initial commit.

---
+++ src/antsibull_docutils/html_utils.py
@@ -0,0 +1,20 @@
+# Author: Felix Fontein <felix@fontein.de>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2024, Ansible Project
+
+"""
+MarkDown utilities.
+"""
+
+from __future__ import annotations
+
+from html import escape as _html_escape
+
+
+def html_escape(text: str) -> str:
+    """
+    Escape a text for HTML.
+    """
+    return _html_escape(text).replace("&quot;", '"')
---
+++ tests/test_html_utils.py
@@ -0,0 +1,31 @@
+# Author: Felix Fontein <felix@fontein.de>
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2024, Ansible Project
+
+"""
+Test html_utils module.
+"""
+
+from __future__ import annotations
+
+import pytest
+
+from antsibull_docutils.html_utils import html_escape
+
+
+@pytest.mark.parametrize(
+    "text, escaped",
+    [
+        ("", ""),
+        ("Test", "Test"),
+        (
+            '<a href="https://example.com/?foo=bar&baz=bam">Test</a>',
+            '&lt;a href="https://example.com/?foo=bar&amp;baz=bam"&gt;Test&lt;/a&gt;',
+        ),
+    ],
+)
+def test_html_escape(text, escaped):
+    result = html_escape(text)
+    assert result == escaped
